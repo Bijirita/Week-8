b35b0bba528a16975370cdc7cd2edf3c1dd2878c
Bijirita <bijirita009@gmail.com>
2018-10-30 15:59:49 -0400

    gulpn

---
+++ project/gulpfile.js
@@ -0,0 +1,20 @@
+var gulp = require('gulp');
+
+var sass = require("gulp-sass");
+
+gulp.task('hello', function() {
+    console.log("Hello Zell");
+})
+
+gulp.task("sass", function() {
+    return gulp.src("app/sass/**/*.sass")
+        .pipe(sass())
+        .pipe(gulp.dest("app/css"))
+});
+
+gulp.task('watch', function(){
+    gulp.watch("app/sass/**/*.sass", ["sass"]);
+
+})
+
+
